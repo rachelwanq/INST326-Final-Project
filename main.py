35ee7614cd2942d0c7f7196a646f5ba0060f173e
Andrea Almeda <akris2902@gmail.com>
2024-04-11 17:50:32 -0400

    add def eat and def read_food_list

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 class Pet():
     def __init__(self, name):
@@ -16,6 +17,21 @@ class Pet():
         self.hunger = int(stats[3])
         self.tiredness = int(stats[4])
         
+    def read_food_list(self, file_name="list_of_foods.txt"):
+        foods = {}
+        food_options = []
+        
+        with open(file_name, "r") as file:
+            for line in file:
+                line = line.strip()
+                if line: 
+                    split_line = re.split(r',\s*', line)
+                    if len(split_line) == 2:
+                        food, health_bonus = split_line
+                        foods[food] = int(health_bonus)
+                        food_options.append(food)
+            
+        return foods, food_options
 
 def random_behavior(filename, pet):
     final_choice = ""
@@ -31,5 +47,24 @@ def random_behavior(filename, pet):
 # if __name__ == "__main__":
 #     dog = Pet("Harry")
 #     print(dog.cleaniness)
-#     random_behavior("randompet.txt", dog)
+#     random_behavior("random_behavior.txt", dog)
 #     print(dog.cleaniness)
+
+def eat(pet, food, file_name="list_of_foods.txt"):
+    food_dict, food_options = pet.read_food_list(file_name)
+    food = food.lower()
+    if food in food_dict:
+        pet.health += food_dict[food]
+        return f"{pet.name} enjoyed the {food} and gained {food_dict[food]}"\
+                " health!"
+    return f"Sorry, {pet.name} doesn't want to eat {food}."
+
+# Testing
+# if __name__ == "__main__":
+#     dog = Pet("Buddy")
+#     foods, food_options = dog.read_food_list()
+#     # print("Available food options:", ", ".join(food_options))
+#     print("Initial health:", dog.health)
+#     message = eat(dog, "orange")
+#     print(message)
+#     print("New health:", dog.health)
